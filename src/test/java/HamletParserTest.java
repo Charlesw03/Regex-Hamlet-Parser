2ddb6d57e6acfbffe5ac07c5a235e9b63fd19306
Charles Wilmer <charleswilmer@zipcoders-MacBook-Pro.local>
2019-03-25 20:05:11 -0400

    completed this ish

--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,17 +16,54 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        String test = "Hamlet likes ham with hams";
+        String wordToFind = "Hamlet";
+        String wordtoChange = "Leon";
+
+        String expected = "Leon likes ham with hams";
+        HamletParser hamletParser = new HamletParser();
+        String actual = hamletParser.replace(wordToFind, wordtoChange, test);
+        Assert.assertEquals(expected, actual);
+
+
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        String test = "Horatio likes ham with hams";
+        String wordToFind = "Horatio";
+        String wordtoChange = "Tariq";
+
+        String expected = "Tariq likes ham with hams";
+        HamletParser hamletParser = new HamletParser();
+        String actual = hamletParser.replace(wordToFind, wordtoChange, test);
+        Assert.assertEquals(expected, actual);
+
     }
 
     @Test
     public void testFindHoratio() {
+    String test = "Horatio has a ratio of two";
+    String wordToFind = "Horatio";
+    Boolean expected = false;
+
+    Boolean actual = hamletParser.find(test,wordToFind);
+
+    Assert.assertEquals(expected ,actual);
+
+
     }
 
     @Test
     public void testFindHamlet() {
+        String test = "Hamlet has a ratio of two";
+        String wordToFind = "Hamlet";
+        Boolean expected = false;
+
+        Boolean actual = hamletParser.find(test,wordToFind);
+
+        Assert.assertEquals(expected ,actual);
+
+
     }
 }
